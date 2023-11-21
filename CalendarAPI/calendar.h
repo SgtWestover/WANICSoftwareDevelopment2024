a34362da45e66e93b4ad5a5f27f1b2487e7e5890
SgtWestover <74578284+SgtWestover@users.noreply.github.com>
2023-11-21 15:20:00 -0800

    started some implementing of the low level calendar stuff

--- CalendarAPI/calendar.h
+++ CalendarAPI/calendar.h
@@ -6,32 +6,60 @@
 #include <iomanip>
 #include <cmath>
 #include <ctime>
+#include <chrono>
 #include <algorithm>
 #include <vector>
 #include <map>
 
-enum Day{Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7};
+// Enum for days of the week
+enum Day { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
 
-class Calendar
+// Enum for months of the year
+enum Month { January = 1, February, March, April, May, June, July, August, September, October, November, December };
+
+// Global map of days in each month
+static const std::map<Month, unsigned char> daysInMonth;
+
+struct Date //Date structure for bundling
+{
+    unsigned char day; //day of the month
+    Month month; //month of the year
+    unsigned int year; //year
+
+    Date(Day _day, Month _month, unsigned int _year) : day(_day), month(_month), year(_year) {}
+};
+
+class Calendar 
 {
 public:
-    Calendar(unsigned long _time = 0);
-    Calendar(unsigned char day = 0, unsigned char month = 0, unsigned int year = 1970);
-    char GetYear() const;
-    void SetYear();
-    char GetMonth() const;
-    void SetMonth();
-    long GetTime() const;
-    void SetTime();
-    Day GetDay() const;
-    void SetDate();
-    bool IsLeapYear() const;
+    Calendar(unsigned long _time = 0); //Default constructor
+    explicit Calendar(const Date& _date); //Constructor using Date struct
+
+    // Getter and setter functions
+    unsigned int GetYear() const;
+    void SetYear(unsigned int _year);
+
+    Month GetMonth() const;
+    void SetMonth(Month _month);
+
+    unsigned long GetTime() const;
     void SetTime(unsigned long _time);
-    void SyncCurrentTime();
+
+    unsigned char GetDay() const;
+    void SetDay(Day _day);
+
+    Day GetDayOfWeek() const;
+    void SetDayOfWeek();
+
+    
+
+    bool IsLeapYear() const; //Returns true if the current year is a leap year
+    void SyncCurrentTime(); //Sets all variables to the current time
+
 private:
-    unsigned long time;
-    unsigned char day;
-    unsigned char month;
-    unsigned int year;
-    Day day;
+    Date TimeToDate(unsigned long _time);
+    Day DateToDay(const Date& _date);
+    unsigned long time; //time in seconds since January 1 1970
+    Date date; //Date structure including the day of the month, the month, and the year
+    Day dayOfWeek; //day of the week
 };
