f79b7d33b9db3cc3d0e2a52af9162d9244f3121a
Kaelin Wang Hu <74578284+SgtWestover@users.noreply.github.com>
2023-11-21 18:12:41 -0800

    oops

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // script.js
 let users = {};
 
+function clearFields() {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+}
+
 function signIn(username, password) {
-    if (users[username] && users[username] === password) 
-    {
-        alert("Sign in successful!");
+    if (users[username]) {
+        if (users[username] === password) {
+            window.location.href = 'index.html';
+        } else {
+            alert("Incorrect password!");
+            clearFields(); // Clear fields if password is incorrect
+        }
     } else {
-        alert("Invalid username or password!");
+        alert("Username does not exist!");
+        clearFields(); // Clear fields if username does not exist
     }
 }
 
@@ -16,12 +26,31 @@ function signUp(username, password) {
     } else {
         users[username] = password;
         alert("Sign up successful!");
+        clearFields(); // Clear fields after successful sign-up
     }
 }
 
 document.getElementById('authForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const username = document.getElementById('username').value;
+    const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
+
+    if (!username || !password) {
+        alert("Please enter both username and password!");
+        return;
+    }
+
     signIn(username, password);
 });
+
+function handleSignUp() {
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value;
+
+    if (!username || !password) {
+        alert("Please enter both username and password for sign up!");
+        return;
+    }
+
+    signUp(username, password);
+}
