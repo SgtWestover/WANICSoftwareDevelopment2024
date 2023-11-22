e9489fbd0aefc4c8939ac130bda72a269373b044
Kaelin Wang Hu <74578284+SgtWestover@users.noreply.github.com>
2023-11-21 18:28:34 -0800

    Added increased password security

--- SignIn.js
+++ SignIn.js
@@ -6,9 +6,24 @@ function clearFields() {
     document.getElementById('password').value = '';
 }
 
+
+function isValidUsername(username) {
+    const usernameRegex = /^[A-Za-z0-9_-]{4,}$/;
+    return usernameRegex.test(username);
+}
+
+function isValidPassword(password) {
+    // Regex to check for at least 8 characters, 1 uppercase, 1 number, and 1 special character
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+
 function signIn(username, password) {
     if (users[username]) {
-        if (users[username] === password) {
+        if (users[username] === password) 
+        {
+            clearFields();
             window.location.href = 'index.html';
         } else {
             alert("Incorrect password!");
@@ -20,7 +35,16 @@ function signIn(username, password) {
     }
 }
 
-function signUp(username, password) {
+function signUp(username, password) 
+{
+    if (!isValidUsername(username)) {
+        alert("Username must be at least 4 characters long and can only contain letters, numbers, underscores, and dashes.");
+        return;
+    }
+    if (!isValidPassword(password)) {
+        alert("Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character.");
+        return;
+    }
     if (users[username]) {
         alert("Username already exists!");
     } else {
