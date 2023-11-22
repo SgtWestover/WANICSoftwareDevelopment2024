5cdf4d926750cef1ea846678e8380d4c897560da
SgtWestover <74578284+SgtWestover@users.noreply.github.com>
2023-11-21 20:18:41 -0800

    Cleaned up password code and added documentation

--- SignIn.js
+++ SignIn.js
@@ -1,80 +1,121 @@
 // script.js
-let users = {};
+let users = {}; //username to password maps
 
-function clearFields() {
-    document.getElementById('username').value = '';
+window.addEventListener('load', clearFields); //clear fields on load or reload
+
+//sets the action, whether sign up or sign in, based on which button is clicked
+function setAction(action) 
+{
+    document.getElementById('action').value = action;
+}
+
+//event listener for both sign up and sign in
+document.getElementById('authForm').addEventListener('submit', function(event) 
+{
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value;
+    const action = document.getElementById('action').value;
+
+    if (!username || !password) 
+    {
+        alert("Please enter both username and password!");
+        return;
+    }
+
+    if (action === 'signIn') 
+    {
+        signIn(username, password);
+    } 
+    else if (action === 'signUp') 
+    {
+        signUp(username, password);
+    }
+});
+
+//clears the password field
+function clearPassword()
+{
     document.getElementById('password').value = '';
 }
 
+//clears both fields
+function clearFields() 
+{
+    document.getElementById('username').value = '';
+    clearPassword();
+}
 
-function isValidUsername(username) {
+//tests whether the username is valid (>3 characters, doesn't contain special characters except for underscore dash)
+function isValidUsername(username) 
+{
     const usernameRegex = /^[A-Za-z0-9_-]{4,}$/;
     return usernameRegex.test(username);
 }
 
-function isValidPassword(password) {
-    // Regex to check for at least 8 characters, 1 uppercase, 1 number, and 1 special character
-    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+//tests whether the password is valid
+function isValidPassword(password, username) 
+{
+    if (password.toLowerCase() === username.toLowerCase()) //failsafe in case future standards change
+    {
+        return false;
+    }
+    // Regex to check for at least 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return passwordRegex.test(password);
 }
 
-
-function signIn(username, password) {
-    if (users[username]) {
-        if (users[username] === password) 
+//sign in function
+function signIn(username, password) 
+{
+    if (users[username]) 
+    {
+        //if successful, go to index.html
+        if (users[username] === password)
         {
             clearFields();
             window.location.href = 'index.html';
-        } else {
-            alert("Incorrect password!");
-            clearFields(); // Clear fields if password is incorrect
         }
-    } else {
+        //otherwise, if incorrect, alert and clear
+        else 
+        {
+            alert("Incorrect password!");
+            clearPassword(); // Clear fields if password is incorrect
+        }
+    }
+    //if username does not exist, clear both fields
+    else 
+    {
         alert("Username does not exist!");
         clearFields(); // Clear fields if username does not exist
     }
 }
 
+//sign up function
 function signUp(username, password) 
 {
-    if (!isValidUsername(username)) {
+    //if username is not valid, alert
+    if (!isValidUsername(username)) 
+    {
         alert("Username must be at least 4 characters long and can only contain letters, numbers, underscores, and dashes.");
         return;
     }
-    if (!isValidPassword(password)) {
-        alert("Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character.");
+    //if password is not valid, alert
+    if (!isValidPassword(password, username)) 
+    {
+        alert("Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character. It cannot match your username");
         return;
     }
-    if (users[username]) {
+    //if the username is already occupied, alert
+    if (users[username]) 
+    {
         alert("Username already exists!");
-    } else {
+    }
+    //otherwise, sign up successfully
+    else 
+    {
         users[username] = password;
         alert("Sign up successful!");
         clearFields(); // Clear fields after successful sign-up
     }
-}
-
-document.getElementById('authForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value;
-
-    if (!username || !password) {
-        alert("Please enter both username and password!");
-        return;
-    }
-
-    signIn(username, password);
-});
-
-function handleSignUp() {
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value;
-
-    if (!username || !password) {
-        alert("Please enter both username and password for sign up!");
-        return;
-    }
-
-    signUp(username, password);
 }
