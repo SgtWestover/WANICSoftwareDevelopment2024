6c56c777ce97970ee93e7cc10f85f61c6ce839a0
SgtWestover <74578284+SgtWestover@users.noreply.github.com>
2023-11-27 23:00:09 -0800

    Added continuity

--- ScheduleTest/DayScheduleFormat.js
+++ ScheduleTest/DayScheduleFormat.js
@@ -24,30 +24,113 @@ function setEndTime(time)
     generateSchedule();
 }
 
-
-function generateSchedule()
+function generateSchedule() 
 {
-    //get the schedule body
+    // Get the schedule body element
     let scheduleBody = document.getElementById('scheduleBody');
-    scheduleBody.innerHTML = '';
-    //generate time blocks
-    for(let i = startTime; i < endTime; i++)
-    {
-        
-        //create a container for a time block element
-        let timeBlockContainer = document.createElement('div');
-        timeBlockContainer.classList.add("time-block-container");
-        timeBlockContainer.style.width = `${100 / (endTime - startTime)}%` //100 / (endTime - startTime);
-        
-        //create a time block element
-        let timeBlock = document.createElement('div');
-        timeBlock.innerHTML = i + 1;
-        timeBlock.classList.add('time-block');
+    
+    // Preserve the timeIndicator if it exists before clearing the scheduleBody
+    let timeIndicator = document.getElementById('timeIndicator');
+    let timeIndicatorExists = timeIndicator !== null;
 
-        //add elements to document    
-        timeBlockContainer.appendChild(timeBlock);
-        scheduleBody.appendChild(timeBlockContainer);
+    // Clear previous content from the schedule body
+    scheduleBody.innerHTML = '';
+
+    // Create a container for the entire day's schedule
+    let dayContainer = document.createElement('div');
+    dayContainer.classList.add("day-container");
+
+    // Generate time blocks for each hour within the day container
+    for (let i = startTime; i < endTime; i++) 
+    {
+        // Create a new time block element
+        let timeBlock = document.createElement('div');
+        timeBlock.innerHTML = i + 1; // Set the time block's display number
+        timeBlock.classList.add('time-block'); // Add the 'time-block' class for styling
+        dayContainer.appendChild(timeBlock); // Append the time block to the day container
+    }
+
+    // Add the day container to the schedule body
+    scheduleBody.appendChild(dayContainer);
+
+    // If the timeIndicator already existed, append it back to the schedule body;
+    // otherwise, create and append a new timeIndicator.
+    if (!timeIndicatorExists) 
+    {
+        timeIndicator = document.createElement('div');
+        timeIndicator.id = 'timeIndicator';
+        timeIndicator.classList.add('time-indicator');
+    }
+    scheduleBody.appendChild(timeIndicator); // Append the time indicator to the schedule body
+}
+
+window.onload = function() 
+{
+    startTime = 0;
+    endTime = 24;
+    generateSchedule();
+};
+
+function setStartTime(input) 
+{
+    let time = parseInt(input, 10); // Parse as an integer to remove leading zeros
+    if (isNaN(time) || time < 0 || time >= endTime) 
+    {
+        console.log("Invalid input for start time.");
+        startTime = 0;
+    } 
+    else 
+    {
+        console.log("Valid start time input.");
+        startTime = time; // Already an integer, no need to parse again
+    }
+    generateSchedule(); // Reflect changes in the UI
+}
+
+function setEndTime(input) 
+{
+    let time = parseInt(input, 10); // Parse as an integer to remove leading zeros
+    if (isNaN(time) || time <= startTime || time > 24) 
+    {
+        console.log("Invalid input for end time.");
+        endTime = 24;
+    } 
+    else 
+    {
+        console.log("Valid end time input.");
+        endTime = time; // Already an integer, no need to parse again
+    }
+    generateSchedule();
+}
+
+// Adjusted function to evaluate on blur
+function evaluateInputOnBlur(inputId) 
+{
+    let inputElement = document.getElementById(inputId);
+    let inputValue = parseInt(inputElement.value, 10);
+    if (isNaN(inputValue) || (inputId === 'startTime' && (inputValue < 0 || inputValue >= endTime)) ||
+        (inputId === 'endTime' && (inputValue <= startTime || inputValue > 24))) 
+    {
+        inputElement.value = inputId === 'startTime' ? 0 : 24; // Set default value if invalid
+    }
+    // Call setStartTime or setEndTime based on which input is being evaluated
+    if(inputId === 'startTime') 
+    {
+        setStartTime(inputElement.value);
+    } 
+    else 
+    {
+        setEndTime(inputElement.value);
     }
 }
 
-
+// Adjusted function to handle Enter keypress
+function handleEnterKeyPress(event, inputId) 
+{
+    if(event.key === 'Enter') 
+    {
+        event.preventDefault();
+        evaluateInputOnBlur(inputId);
+        event.target.blur(); 
+    }
+}
