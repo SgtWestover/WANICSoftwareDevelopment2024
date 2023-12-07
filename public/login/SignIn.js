d8e51e9a0a67e266e8600f880d357dc704103bfa
SgtWestover <74578284+SgtWestover@users.noreply.github.com>
2023-12-06 17:33:20 -0800

    test

--- public/login/SignIn.js
+++ public/login/SignIn.js
@@ -92,6 +92,7 @@ function pull() {
             // Implement actions for other types of failures
         }
     })
+    //test
     .catch(error => {
         // This catches network errors and errors thrown from response processing
         console.error('There was a problem with the fetch operation: ', error.message);
